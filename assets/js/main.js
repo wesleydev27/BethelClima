06d3efe5dc2a706bf2c098e0d4d8dbb1a3571b1d
Wesley Vaz <72579226+wesleydev27@users.noreply.github.com>
2025-03-16 22:44:33 -0300

    change project

---
+++ assets/js/main.js
@@ -0,0 +1,25 @@
+// SLIDER com letras
+document.addEventListener("DOMContentLoaded", function () {
+    const slides = document.querySelectorAll(".slide");
+    let currentIndex = 0;
+
+    function showSlide(index) {
+        slides.forEach((slide, i) => {
+            slide.classList.remove("active");
+            if (i === index) {
+                slide.classList.add("active");
+            }
+        });
+    }
+
+    function nextSlide() {
+        currentIndex = (currentIndex + 1) % slides.length;
+        showSlide(currentIndex);
+    }
+
+    showSlide(currentIndex);
+    setInterval(nextSlide, 10000);
+});
+
+/*Init library AOS*/
+AOS.init();
---
+++ assets/js/menu.js
@@ -0,0 +1,12 @@
+/*menu mobile*/
+const menuBtn = document.getElementById("menu-btn");
+const closeMenu = document.getElementById("close-menu");
+const mobileMenu = document.getElementById("mobile-menu");
+
+menuBtn.addEventListener("click", () => {
+    mobileMenu.classList.remove("hidden");
+});
+
+closeMenu.addEventListener("click", () => {
+    mobileMenu.classList.add("hidden");
+});
